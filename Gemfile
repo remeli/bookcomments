ee711e209ae8509711f5ee0b08ece29343413188
Kusainov Yerlan <lagox.mail@gmail.com>
2011-11-07 09:53:26 +0600

    first commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails', '~> 3.1'
+gem 'capistrano'
+gem 'mysql2'
+gem 'json'
+gem 'sass-rails'
+gem 'therubyracer'
+gem 'uglifier'
+gem 'jquery-rails'
+gem 'coffee-rails'
+gem "rake", "~> 0.9.2"
+gem 'kaminari'
+gem 'unicorn'
+gem 'activeadmin'
+gem "meta_search", '~> 1.1.1'
+gem "acts_as_tree", "~> 0.1.1"
+gem "paperclip", "~> 2.4"
+gem "zurb-foundation"
+group :development do
+  gem 'rails-dev-tweaks', '~> 0.5.1'
+  gem 'awesome_print'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookcomments::Application.config.secret_token = 'bbc419c006f9767d557961a07745cd3fcf8f8737f37d867ee791372f30415f18b27027e47658b5d15387c1c99aacb13777e274feb102d44d6e2896147a6a4999'

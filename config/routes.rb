75891198c58075375cb43a48125e0298e775e9e2
Kusainov Yerlan <lagox.mail@gmail.com>
2011-11-07 10:24:12 +0600

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Bookcomments::Application.routes.draw do
+  
+  # pages:
+  match "/about" => "pages#about"
+  match "/contacts" => "pages#contacts"
+  match "/licence" => "pages#licence"
+    
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +54,7 @@ Bookcomments::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#index'
 
   # See how all your routes lay out with "rake routes"
 

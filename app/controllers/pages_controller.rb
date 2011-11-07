54d61a8f98c946ac2e059733118266fc8a1a8da0
Kusainov Yerlan <lagox.mail@gmail.com>
2011-11-07 10:21:11 +0600

    edit pagescontroller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,17 @@
 class PagesController < ApplicationController
   def index
-    
+    @title = "Главная"
   end
   
   def about
-    
+    @title = "О нас"
   end
   
   def contacts
-    
+    @title = "Контакты"
   end
   
   def license
-    
+    @title = "Лицензионное соглашение"
   end
 end

a0d344af7fdcbbfa11eda8ee1fb5c899bcfa8981
Kusainov Yerlan <lagox.mail@gmail.com>
2011-11-07 18:56:35 +0600

    cateogry model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,11 @@
 class Category < ActiveRecord::Base
+  
+  acts_as_tree :order => "title"
+  
+  # validates:
+  validates :title, :presence => true
+  validates :permalink, :presence => true
+  
+  # scopes:
+  default_scope order("title ASC")
 end

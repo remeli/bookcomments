ef686fe547eefe43b295bfeaa7c54d65e28fb082
Kusainov Yerlan <lagox.mail@gmail.com>
2011-11-08 13:04:41 +0600

    fill book controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,11 @@
 class BooksController < ApplicationController
+  def index
+    @title = "Книги"
+    @books = Book.all
+  end
+  
+  def show
+    @book = Book.find(params[:id])
+    @title = @book.title
+  end
 end

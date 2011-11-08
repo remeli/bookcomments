5514b2cf8b009e29fc0608116e6a9d5e6c8dfb93
Kusainov Yerlan <lagox.mail@gmail.com>
2011-11-08 14:06:34 +0600

    book->paperclip

--- app/models/book.rb
+++ app/models/book.rb
@@ -13,5 +13,11 @@ class Book < ActiveRecord::Base
   #scope
   default_scope order("created_at ASC")
   
+  #paperclip:
+  has_attached_file :cover, :styles => {:medium => "200x275>", :thumb => "125x170>" },
+      :url => "/system/:attachment/:id/:style/:basename.:extension",
+      :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension"
+  validates_attachment_size :cover, :less_than => 5.megabytes
+  validates_attachment_content_type :cover, :content_type => ['image/jpeg', 'image/png', 'image/gif']
   
 end

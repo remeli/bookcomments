77c82d22e31bd3d6913e35b236e00fd44b3a300d
Kusainov Yerlan <lagox.mail@gmail.com>
2011-11-08 11:14:58 +0600

    show action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,7 @@
 class CategoriesController < ApplicationController
-  
+  def show
+    @category = Category.find(params[:id])
+    @title = @category.title
+    @description = @category.description
+  end
 end

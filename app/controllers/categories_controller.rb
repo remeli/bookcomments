b196da161bd724330bd49e09129724148bbbe422
Kusainov Yerlan <lagox.mail@gmail.com>
2011-11-08 11:16:50 +0600

    description, keys add to layout

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,11 @@
 class CategoriesController < ApplicationController
+
   def show
+    # base:
     @category = Category.find(params[:id])
     @title = @category.title
     @description = @category.description
+    
   end
+  
 end

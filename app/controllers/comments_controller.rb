bb8c93ad476627ee8102f381080f0ae363a41dd6
Kusainov Yerlan <lagox.mail@gmail.com>
2011-11-09 23:57:13 +0600

    comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+  before_filter :load_book
+  
+  def create
+    @comment = @book.comment.new(params[:comment])
+    if @comment.save
+      redirect_to @comment, :notice => "Комментарий успешно добавлен"
+    else
+      redirect_to @comemnt, :alert => "Комментарий не добавлен"
+    end
+  end
+  
+  private
+    def load_book
+      @book = Book.find(params[:book_id])
+    end
 end

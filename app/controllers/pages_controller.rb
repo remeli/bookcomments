d576a1645251ce3c617f5fa15e91181b64f5551c
Kusainov Yerlan <lagox.mail@gmail.com>
2011-11-17 18:28:27 +0600

    alphabet start

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,11 @@ class PagesController < ApplicationController
   def index
     @title = "Главная"
     @tenbooks = Book.reorder("created_at DESC").limit(10)
+    @alphabet = [
+"0-9","А","Б","В","Г","Д","Е","Ж","З","И","Й","К","М","Н","О","П","Р","С","Т","У","Ф","Х","Ц","Ч","Ш","Щ","Э","Ю","Я"
+    ]
   end
-  
+
   def about
     @title = "О нас"
   end

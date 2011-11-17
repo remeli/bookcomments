4a52f4799ad179617c9c11323df155e1f98a8a01
Kusainov Yerlan <lagox.mail@gmail.com>
2011-11-17 11:35:29 +0600

    search finish

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,25 +5,34 @@ class SearchController < ApplicationController
     if params[:keyword].blank?
       redirect_to(root_path, :alert => "Ничего не найдено")
     else      
+      
       word = '%'+params[:keyword]+'%'
       topic = params[:topic]
+      
       if topic == "books"
         
         text_conditions = "SELECT * FROM books WHERE (title like ? or authorname like ?)"
         conditions = [text_conditions, word, word]
         @books = Book.find_by_sql(conditions)
+        @books = Kaminari.paginate_array(@books).page(params[:page]).per(9)
         result(@books, "books")
+        
       elsif topic == "authors"
+        
         text_conditions = "SELECT * FROM authors WHERE (name like ?)"
         conditions = [text_conditions, word]
         @authors = Author.find_by_sql(conditions)
+        @authors = Kaminari.paginate_array(@authors).page(params[:page]).per(12)
         result(@authors, "authors")
         
       elsif topic == "articles"
+        
         text_conditions = "SELECT * FROM articles WHERE (title like ? or body like ?)"
         conditions = [text_conditions, word, word]
         @articles = Article.find_by_sql(conditions)
+        @articles = Kaminari.paginate_array(@articles).page(params[:page]).per(5)
         result(@articles, "articles")
+        
       else
         redirect_to(root_path, :alert => "Ничего не найдено")
       end      
@@ -39,4 +48,3 @@ class SearchController < ApplicationController
   end
   
 end
-# todo: сделать kaminari, доделть articles view, добавить в книгах h1 и тд

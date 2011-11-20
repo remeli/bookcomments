1f89ef24498ce2fa397d543465d81c78188e698f
Kusainov Yerlan <lagox.mail@gmail.com>
2011-11-20 17:18:57 +0600

    upd authors books

--- app/models/author.rb
+++ app/models/author.rb
@@ -12,4 +12,18 @@ class Author < ActiveRecord::Base
       :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension"
   validates_attachment_size :photo, :less_than => 5.megabytes
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/gif']
+
+  after_save :updatebooks
+  after_update :updatebooks
+  
+  private
+  
+    def updatebooks
+      @books = Book.where(:authorname => name)
+      for book in @books do
+        if book.author_id == nil
+          book.update_attributes(:author_id => id)
+        end
+      end
+    end
 end

88933accdc1930775137458b4218c7127d919084
Kusainov Yerlan <lagox.mail@gmail.com>
2011-11-22 11:02:13 +0600

    ten authors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   
   before_filter :load_categories, :load_child_categories
   before_filter :load_articlecategories
-  # before_filter :load_random_ten_authors
+  before_filter :load_random_ten_authors
   def load_categories
     @mains_categories = Category.mains
   end
@@ -14,13 +14,13 @@ class ApplicationController < ActionController::Base
     @child_categories = Category.childrens.order("title ASC")
   end
   
-  # def load_random_ten_authors
-  #   rand_count = 0
-  #   while rand_count == 0
-  #     rand_count = rand(Author.count-10)
-  #   end
-  #   @rand_authors = Author.limit(10).offset(rand_count)
-  # end
+  def load_random_ten_authors
+    rand_count = 0
+    while rand_count == 0
+      rand_count = rand(Author.all.count-10)
+    end
+    @rand_authors = Author.limit(10).offset(rand_count)
+  end
   
   def load_articlecategories
     @myarticlecategories = Articlecategory.all

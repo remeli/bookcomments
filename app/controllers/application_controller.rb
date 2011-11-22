2930327d7221d0e9e973cc887cfaf7511e812436
Kusainov Yerlan <lagox.mail@gmail.com>
2011-11-22 10:13:29 +0600

    ten random authors delete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  before_filter :load_categories, :load_child_categories, :load_random_ten_authors
+  before_filter :load_categories, :load_child_categories
   before_filter :load_articlecategories
-  
+  # before_filter :load_random_ten_authors
   def load_categories
     @mains_categories = Category.mains
   end
@@ -14,13 +14,13 @@ class ApplicationController < ActionController::Base
     @child_categories = Category.childrens.order("title ASC")
   end
   
-  def load_random_ten_authors
-    rand_count = 0
-    while rand_count == 0
-      rand_count = rand(Author.count-10)
-    end
-    @rand_authors = Author.limit(10).offset(rand_count)
-  end
+  # def load_random_ten_authors
+  #   rand_count = 0
+  #   while rand_count == 0
+  #     rand_count = rand(Author.count-10)
+  #   end
+  #   @rand_authors = Author.limit(10).offset(rand_count)
+  # end
   
   def load_articlecategories
     @myarticlecategories = Articlecategory.all
